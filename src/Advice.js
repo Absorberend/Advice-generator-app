ecef94637e16a661b6266f54a6893458b0d87c69
Absorberend <102243063+Absorberend@users.noreply.github.com>
2023-02-01 17:00:33 +0100

    responsive design done

--- src/Advice.js
+++ src/Advice.js
@@ -1,18 +1,25 @@
 import './Advice.scss';
 import diceIcon from "./assets/icon-dice.svg";
 import mobileDivider from "./assets/pattern-divider-mobile.svg";
+import desktopDivider from "./assets/pattern-divider-desktop.svg";
+import { useMediaQuery } from 'react-responsive';
+
 
 export default function Advice() {
+  const isDesktop = useMediaQuery({ query: '(min-width: 600px)' });
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 599px)' });
+
   return (
     <section className="advice">
         <div className="advice__number">Advice #117</div>
-        <div className="advice__quote">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec quam ac ante luctus gravida. "</div>
-        <div>
-            <img src={mobileDivider} />
+        <div className="advice__quote">"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec quam ac ante. "</div>
+        <div className="advice__divider">
+            {isTabletOrMobile && <img src={mobileDivider} alt="" />}
+            {isDesktop && <img src={desktopDivider} alt="" />}
         </div>
         <div className="advice__dice">
             <div>
-                <img src={diceIcon} />
+                <img src={diceIcon} alt="" />
             </div>
         </div>
     </section>
